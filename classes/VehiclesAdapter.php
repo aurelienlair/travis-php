98700c93dc216792e0c712ec9f71664f8a796127
aurelienlair <aurelien.lair@onebip.com>
2017-12-05 23:08:33 +0000

    tests: fixing query wrong table name

--- classes/VehiclesAdapter.php
+++ classes/VehiclesAdapter.php
@@ -11,7 +11,7 @@ class VehiclesAdapter
 
     public function countByColor($color)
     {
-        $sql = "SELECT count(1) FROM vehicles WHERE color = " . $this->pdo->quote($color);
+        $sql = "SELECT count(1) FROM vehicle WHERE color = " . $this->pdo->quote($color);
         $stmt = $this->pdo->query($sql);
         return count($stmt->fetchColumn());
     }
