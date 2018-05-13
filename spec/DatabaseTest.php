75afc689f4d6dac4b869d796442778110923937b
Aurélien Lair <aurelien.lair@gmail.com>
2018-05-13 14:21:42 +0200

    php: refactoring, cleaning code

--- spec/DatabaseTest.php
+++ spec/DatabaseTest.php
@@ -5,16 +5,22 @@ use PHPUnit\Framework\TestCase;
 class DatabaseTest extends TestCase
 {
     private $pdo;
+    private $vehiclesAdapter;
 
     public function setUp()
     {
-        $this->pdo = new \PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo = new \PDO(
+            $GLOBALS['db_dsn'], 
+            $GLOBALS['db_username'], 
+            $GLOBALS['db_password']
+        );
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->vehiclesAdapter = new VehiclesAdapter($this->pdo);
     }
 
     public function testCountVehiclesByColor()
     {
+        $this->assertEquals(2, $this->vehiclesAdapter->countByColor('Red'));
         $vehiclesAdapter = new VehiclesAdapter($this->pdo);
-        $this->assertEquals(3, $vehiclesAdapter->countByColor('red'));
     }
 }
